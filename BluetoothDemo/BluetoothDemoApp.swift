6be3f3e7e37d1d22ff26d0187ea6ef080c56a3df
Maxim Tarasov <mtarasov@biolinq.com>
2025-08-25 15:31:52 -0700

    Initial Commit

---
+++ BluetoothDemo/BluetoothDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothDemoApp.swift
+//  BluetoothDemo
+//
+//  Created by Maxim Tarasov on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothDemoTests/BluetoothDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  BluetoothDemoTests.swift
+//  BluetoothDemoTests
+//
+//  Created by Maxim Tarasov on 8/25/25.
+//
+
+import Testing
+
+struct BluetoothDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
